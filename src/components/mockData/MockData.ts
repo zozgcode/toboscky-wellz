adeca3e7a267315f6e23bff73965deb6b7dc581c
sunshine-web-coder <webcoderdev02@gmail.com>
2024-09-05 14:23:19 +0100

    mnnj

--- src/components/mockData/MockData.ts
+++ src/components/mockData/MockData.ts
@@ -606,7 +606,7 @@ export const mockAccounts: Account[] = [
       lastName: "",
       jointNames: "",
       username: "McDanielAA",
-      password: "GoodGod26!37@190000000000000000000000000007",
+      password: "GoodGod26!37!",
     },
     bank_details: {
       account_type: "Checking",
