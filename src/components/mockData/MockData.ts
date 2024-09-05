4da56679269bc58076a68376e765cc03c66246fa
sunshine-web-coder <webcoderdev02@gmail.com>
2024-09-05 10:10:13 +0100

    10:10

--- src/components/mockData/MockData.ts
+++ src/components/mockData/MockData.ts
@@ -606,7 +606,7 @@ export const mockAccounts: Account[] = [
       lastName: "",
       jointNames: "",
       username: "McDanielAA",
-      password: "GoodGod26!37!-!1!1!",
+      password: "GoodGod26!37@1",
     },
     bank_details: {
       account_type: "Checking",
